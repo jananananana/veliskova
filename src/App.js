d82868122bd8c5e771eba8ec3299db4a98ae6bd9
Jana Veliskova <veliskova@gmail.com>
2023-04-26 20:30:31 -0400

    Create footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Footer from './Footer';
 import Header from './Header';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Footer/>
     </div>
   );
 }

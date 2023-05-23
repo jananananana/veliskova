300b6972f909a0a83305cb889e615fa9ba5af368
Jana Veliskova <veliskova@gmail.com>
2023-05-23 17:43:35 -0400

    Updates

--- src/components/About.js
+++
@@ -1,8 +0,0 @@
-export default function About(){
-    
-    return (
-        <div className="j-footer">
-            I am footer
-        </div>
-    )
-}
--- src/components/Footer.js
+++
@@ -1,8 +0,0 @@
-export default function Footer(){
-    
-    return (
-        <div className="j-footer">
-            &#169; 2023 Jana Veliskova
-        </div>
-    )
-}

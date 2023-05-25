8884fa06449a443222fad6d2f257e5cfc808e2e4
Jana Veliskova <veliskova@gmail.com>
2023-05-25 17:30:40 -0400

    updating colors, styling changes, routes

---
+++ src/components/Blog.js
@@ -0,0 +1,11 @@
+export default function Blog(){
+    return (
+        <header className="j-header">
+            <div>
+                Jana Veliskova
+            </div>
+
+            
+        </header>
+    )
+}

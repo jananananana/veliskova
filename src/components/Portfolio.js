61e256677cec13258945c27073811a04ced0d349
Jana Veliskova <veliskova@gmail.com>
2023-08-17 15:53:03 -0400

    update package.json

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,29 +1,28 @@
-import { CreateHash } from 'sha.js';
-
+import React, { useState } from 'react';
+import * as shajs from 'sha.js'; // Import the sha.js library
 
  
-// console.log(shajs('sha256').update('42').digest('hex'))
-// // => 73475cb40a568e8da8a045ced110137e159f890ac4da883b6b17dc651b3a8049
-// console.log(new shajs.sha256().update('42').digest('hex'))
-// // => 73475cb40a568e8da8a045ced110137e159f890ac4da883b6b17dc651b3a8049
- 
-// var sha256stream = shajs('sha256')
-// sha256stream.end('42')
-// console.log(sha256stream.read().toString('hex'))
+console.log(shajs('sha512').update('StarTrekTNGisBetterThanStarWars').digest('hex'))
+
+
 
 export default function Portfolio(){
+const portfoliopass = '8af306bb69bb8d6420ce1f744deb8f62a159cec1fc1028398c3cd74f28e3488591bdd00e1190f0e977d249c4e3f27a854c1cc72cd8dea52846e625c75318073e';
+const [shouldLoadComponent, setShouldLoadComponent] = useState(true);
+
     return (
-        <header className="j-header">
-            <div>
-                Jana Veliskova
+        <main className="j-home">
+            <div className="j-home__desc">
+                <label htmlFor="pass-input">Enter Password to View This Area</label>
+                <input id= "pass-input" type="text" />
             </div>
-            <div sha512='a0b6a741d3a647f0f76ed9456b0a96300aafe27f72b9fbf4df0b859d74cbe9f2c7f74017e8a3f69b08f4d3be3b3e1c370e00e9f8cb3c5a26d2d4a01b4a9d522e'>
+            <div sha512='8af306bb69bb8d6420ce1f744deb8f62a159cec1fc1028398c3cd74f28e3488591bdd00e1190f0e977d249c4e3f27a854c1cc72cd8dea52846e625c75318073e'>
                 <div>
-                    TEST CONTENT
+                    SHOULD BE HIDDEN
+                   
                 </div>
             </div>
+        </main>
 
-            
-        </header>
     )
 }

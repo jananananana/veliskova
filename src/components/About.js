0655fae994d11e46db81e12b9a2c449d630e0474
Jana Veliskova <veliskova@gmail.com>
2023-09-11 16:09:50 -0400

    add content

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,30 @@ export default function About(){
     return (
         <main className="j-home">
             <div className="j-home__desc">
-                This site is undergoing some construction.
+                <ul className="j-home__experiences">
+                    <li className="j-home__experience-item">
+                        <h2>
+                            About Jana
+                        </h2>
+                    </li>
+                    <li className="j-home__experience-item">
+                        <div>
+                            With over a decade of experience spanning product management, UX, and front-end engineering, Jana brings a multifaceted skill set to the tech industry.
+                            She has extensive experience in the product development lifecycle, specializing in delivering products iteratively in close collaboration with engineering, product, design, stakeholders, and customers.
+                        </div>
+                        <div>
+                            Her approach is rooted in both qualitative and quantitative data analysis, ensuring that her decisions are not just informed but also well-documented for scalable growth.
+                        </div>
+                        <div>
+                            An advocate for community involvement, Jana is a regular at various tech meetups and conferences, having spoken at events like UX Bar Camp DC, Philly Tech Week, PANMA, Code for American, and the Front-End Philly Meetup. She also led the Girl Develop It Philadelphia book club, aimed at empowering women in tech.
+                        </div>
+                        <div>
+                            Jana holds a BA in History from <a href="https://www.cornell.edu/search/people.cfm?netid=jv99">Cornell University</a>.
+                        </div>
+
+
+                    </li>
+                </ul>
             </div>
             <div className="j-home__desc">
                 <ul className="j-home__experiences">
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -12,15 +12,59 @@ const [shouldLoadComponent, setShouldLoadComponent] = useState(true);
 
     return (
         <main className="j-home">
-            <div className="j-home__desc">
-                <label htmlFor="pass-input">Enter Password to View This Area</label>
-                <input id= "pass-input" type="text" />
+            {/* <div className="j-home__pass-area">
+                <label className="j-home__label" htmlFor="pass-input">Enter Password to View This Area</label>
+                <input className="j-home__input" id= "pass-input" type="text" />
             </div>
             <div sha512='8af306bb69bb8d6420ce1f744deb8f62a159cec1fc1028398c3cd74f28e3488591bdd00e1190f0e977d249c4e3f27a854c1cc72cd8dea52846e625c75318073e'>
                 <div>
-                    SHOULD BE HIDDEN
+                    
                    
                 </div>
+            </div> */}
+            <div>
+            <div className="portfolio-container">
+
+      {/* Clients Section */}
+      <section className="clients-section">
+        <h2>Clients</h2>
+        <section>My clients have included the following either through full-time employment or consulting work.</section>
+        <ul>
+          <li>Client 1</li>
+          <li>Client 2</li>
+          <li>Client 3</li>
+          <li>Client 4</li>
+        </ul>
+      </section>
+
+      {/* Work Samples Section */}
+      <section className="work-samples-section">
+        <h2>Work Samples</h2>
+        <div className="work-sample">
+          <h3>Project 1</h3>
+          <p>Description of project 1</p>
+        </div>
+        <div className="work-sample">
+          <h3>Project 2</h3>
+          <p>Description of project 2</p>
+        </div>
+        <div className="work-sample">
+          <h3>Project 3</h3>
+          <p>Description of project 3</p>
+        </div>
+      </section>
+
+      {/* Speaking Engagements Section */}
+      <section className="speaking-engagements-section">
+        <h2>Speaking Engagements</h2>
+        <ul>
+          <li>Conference 1 - Topic 1</li>
+          <li>Conference 2 - Topic 2</li>
+          <li>Webinar 1 - Topic 3</li>
+        </ul>
+      </section>
+    </div>
+
             </div>
         </main>
 

a0447d9f7a2da05fa20d0c6c581c1ddfd8058d3d
Jana Veliskova <veliskova@gmail.com>
2024-08-14 11:44:10 -0400

    styling things

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,13 +6,16 @@ import { Link } from 'react-router-dom';
 export default function Header(){
     return (
         <header className='top-header'>
-            <h1 className="top-header__left b-heading--1">
-                <Link to={'/'} className='j-button--plain-inverse'> Hi! I'm <span className='b-heading--1__highlight'>Jana</span></Link>
-               
-            </h1>
+           <div className='top-header__container'>
 
-            <Navbar/>
+                <h1 className="top-header__left b-heading--1">
+                    <Link to={'/'} className='j-button--plain-inverse'> Hi! I'm <span className='b-heading--1__highlight'>Jana</span></Link>
+                
+                </h1>
 
+                <Navbar/>
+           </div>
+  
         </header>
     )
 }
